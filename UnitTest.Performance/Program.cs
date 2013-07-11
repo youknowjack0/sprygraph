616ceeb26a35dd222950901e516eda8f51da2bf1
Jack Langman <jlangman@alastri.com.au>
2013-07-11 16:19:22 +0800

    comparison code

--- UnitTest.Performance/Program.cs
+++ UnitTest.Performance/Program.cs
@@ -14,26 +14,36 @@ namespace UnitTest.Performance
     class Program
     {
         static void Main(string[] args)
+        {            
+            
+
+            BasicDijkstra();
+            
+            int coldcalls = 10;
+
+            Coldcalls(coldcalls);
+
+            Spatial(coldcalls);
+        }
+
+        private static void BasicDijkstra()
         {
 
+            var sw = new Stopwatch();
             var rg = QuickGraphComparisons.GenerateRandomGraph(2000, 2);
             Random r = new Random();
-            
-            var sw = new Stopwatch();
-            /*
             sw.Restart();
             {
                 GraphReader<TestVertex, TestEdge> sgreader = new GraphReader<TestVertex, TestEdge>(rg);
                 foreach (var source in rg.VerticesList)
                 {
-                    
-                    PathFinder<TestVertex, TestEdge> sgsolver = sgreader.GetPathFinder(source);
+                    DijkstraPathFinder<TestVertex, TestEdge> sgsolver = sgreader.GetDijkstraPathFinder(source);
 
                     foreach (var v in rg.VerticesList)
                     {
                         TestEdge[] sgresult;
                         bool sggot = sgsolver.TryGetPath(v, out sgresult);
-                    }                    
+                    }
                 }
             }
             sw.Stop();
@@ -51,14 +61,16 @@ namespace UnitTest.Performance
                         bool qggot = qgsolver(v, out qgresult);
                     }
                 }
-
             }
             sw.Stop();
             Console.WriteLine("Quickgraph took " + sw.ElapsedMilliseconds);
-            */
-            
+        }
 
-            int coldcalls = 10000;
+        private static int Coldcalls(int coldcalls)
+        {
+            var sw = new Stopwatch();
+            var rg = QuickGraphComparisons.GenerateRandomGraph(200000, 2);
+            Random r = new Random();
 
             List<TestVertex> randomSources = new List<TestVertex>(coldcalls);
             List<TestVertex> randomDestinations = new List<TestVertex>(coldcalls);
@@ -74,17 +86,17 @@ namespace UnitTest.Performance
                 for (int index = 0; index < randomSources.Count; index++)
                 {
                     var source = randomSources[index];
-                    var destination = randomDestinations[index];                    
+                    var destination = randomDestinations[index];
                     DijkstraPathFinder<TestVertex, TestEdge> sgsolver = sgreader.GetDijkstraPathFinder(source);
                     TestEdge[] sgresult;
                     bool sggot = sgsolver.TryGetPath(destination, out sgresult);
                 }
             }
             sw.Stop();
-            Console.WriteLine("Sprygraph cold-query Dijkstra time: " + (double)sw.ElapsedMilliseconds/(coldcalls));
-            
+            Console.WriteLine("Sprygraph cold-query Dijkstra time: " + (double) sw.ElapsedMilliseconds/(coldcalls));
+
             sw.Restart();
-            {                
+            {
                 for (int index = 0; index < randomSources.Count; index++)
                 {
                     var source = randomSources[index];
@@ -96,8 +108,23 @@ namespace UnitTest.Performance
                 }
             }
             sw.Stop();
-            Console.WriteLine("Quickgraph cold-query Dijkstra time: " + (double)sw.ElapsedMilliseconds / (coldcalls));
+            Console.WriteLine("Quickgraph cold-query Dijkstra time: " + (double) sw.ElapsedMilliseconds/(coldcalls));
+            return coldcalls;
+        }
 
+        private static void Spatial(int coldcalls)
+        {
+            var sw = new Stopwatch();
+            var rg = QuickGraphComparisons.GenerateRandomGraph2(1000);
+            Random r = new Random();            
+
+            List<TestVertex> randomSources = new List<TestVertex>(coldcalls);
+            List<TestVertex> randomDestinations = new List<TestVertex>(coldcalls);
+            for (int i = 0; i < coldcalls; i++)
+            {
+                randomSources.Add(rg.VerticesList[r.Next(0, rg.VertexCount - 1)]);
+                randomDestinations.Add(rg.VerticesList[r.Next(0, rg.VertexCount - 1)]);
+            }
 
             sw.Restart();
             {
@@ -112,22 +139,23 @@ namespace UnitTest.Performance
                 }
             }
             sw.Stop();
-            Console.WriteLine("Sprygraph cold-query A* time: " + (double)sw.ElapsedMilliseconds / (coldcalls));
+            Console.WriteLine("Sprygraph cold-query A* time: " + (double) sw.ElapsedMilliseconds/(coldcalls));
 
             sw.Restart();
-            {                
+            {
                 for (int index = 0; index < randomSources.Count; index++)
                 {
                     var source = randomSources[index];
                     var destination = randomDestinations[index];
-                    TryFunc<TestVertex, IEnumerable<TestEdge>> qgsolver = rg.ShortestPathsAStar (x => x.GetCost(), x => x.Heuristic(destination),
-                                                                                                   source);
+                    TryFunc<TestVertex, IEnumerable<TestEdge>> qgsolver = rg.ShortestPathsAStar(x => x.GetCost(),
+                                                                                                 x => x.Heuristic(destination),
+                                                                                                 source);
                     IEnumerable<TestEdge> qgresult;
                     bool qggot = qgsolver(destination, out qgresult);
                 }
             }
             sw.Stop();
-            Console.WriteLine("Quickgraph cold-query A* time: " + (double)sw.ElapsedMilliseconds / (coldcalls));
+            Console.WriteLine("Quickgraph cold-query A* time: " + (double) sw.ElapsedMilliseconds/(coldcalls));
         }
     }
 }
--- UnitTestProject1/QuickGraphComparisons.cs
+++ UnitTestProject1/QuickGraphComparisons.cs
@@ -110,7 +110,56 @@ namespace UnitTestProject1
                 }
             }
         }
-  
+
+        [Test]
+        public void AStarSpatial()
+        {
+            var rg = GenerateRandomGraph2(15);
+
+
+            GraphReader<TestVertex, TestEdge> sgreader = new GraphReader<TestVertex, TestEdge>(rg);
+
+            Random r = new Random();
+            foreach (var v in rg.VerticesList)
+            {
+                AStarPathFinder<TestVertex, TestEdge> sgsolver = sgreader.GetAStarPathFinder(v);
+                TryFunc<TestVertex, IEnumerable<TestEdge>> qgsolver = rg.ShortestPathsAStar(x => x.GetCost(), x => x.Heuristic(x), v);
+                foreach (var vt in rg.VerticesList)
+                {
+                    IEnumerable<TestEdge> qgresult;
+                    bool qggot = qgsolver(vt, out qgresult);
+                    TestEdge[] sgresult;
+                    bool sggot = sgsolver.TryGetPath(vt, out sgresult);
+
+                    if (v == vt) //quickgraph???
+                    {
+                        //Assert.True(qggot == false && sggot == true);
+                        Assert.True(sggot && sgresult.Length == 0);
+                        continue;
+                    }
+
+                    TestEdge[] qgresultarray = null;
+                    if (qggot && sggot)
+                    {
+                        qgresultarray = qgresult as TestEdge[] ?? qgresult.ToArray();
+                        _qglen = qgresultarray.Aggregate(0.0, (sum, edge) => sum + edge.GetCost());
+                        _sglen = sgresult.Aggregate(0.0, (sum, edge) => sum + edge.GetCost());
+
+                    }
+                    Assert.True(qggot == sggot);
+                    if (qggot)
+                    {
+                        int i = 0;
+                        foreach (var item in qgresultarray)
+                        {
+                            Assert.True(item == sgresult[i]);
+                            i++;
+                        }
+                        Assert.True(sgresult.Length == i);
+                    }
+                }
+            }
+        }
 
         public  static IHybridGraph GenerateRandomGraph(int vertices, int degree)
         {
@@ -136,11 +185,83 @@ namespace UnitTestProject1
                     int n = r.Next(0, vertices);
 
                     @out.Add(vl[n]);
+                    
                 }
                 var x = item.OutEdges; //called for effect
+                foreach (var e in x)
+                {
+                    el.Add(e);
+                }
             }
 
             return new TestGraph(vl, el);
+        }
+    
+        /// <summary>
+        /// generate a more regular spatial graph, 2d
+        /// </summary>        
+        public  static IHybridGraph GenerateRandomGraph2(int sideCount)
+        {
+            directions = new List<int[]>();
+            directions.Add(new int[]{0,1});
+            directions.Add(new int[] { 0, -1 });
+            directions.Add(new int[] { 1, 0 });
+            directions.Add(new int[] { -1, 0 });
+            directions.Add(new int[] { 1, 1 });
+            directions.Add(new int[] { -1, 1 });
+            directions.Add(new int[] { 1, -1 });
+            directions.Add(new int[] { -1, -1 });
+            var vl = new List<TestVertex>();
+            var el = new List<TestEdge>();
+
+            TestVertex[,] matrix = new TestVertex[sideCount,sideCount];
+
+            Random r = new Random();
+
+
+            for (int i = 0; i < sideCount; i++)
+            {                
+                for (int j = 0; j < sideCount; j++)
+                {
+                    var vertex = new TestVertex(i + (r.NextDouble() - 0.5) / 2, j + (r.NextDouble() - 0.5) / 2, 0); //jitter to make multiple valid paths unlikely (easier for testing)
+                    matrix[i,j] = vertex;
+                    vl.Add(vertex);
+                }
+            }
+
+
+
+            for (int i = 1; i < sideCount - 1;i++)
+            {
+                for (int j = 1; j < sideCount - 1; j++)
+                {
+                    var v = matrix[i,j];
+                    var @out = v.Out;
+                    ConnectRandomly(v, matrix, i, j, r, @out);
+                    var x = v.OutEdges; //called for effect
+                    foreach (var e in x)
+                    {
+                        el.Add(e);
+                    }
+                }
+                
+            }
+
+            return new TestGraph(vl, el);
+        }
+
+        private static List<int[]> directions; 
+
+        private static void ConnectRandomly(TestVertex testVertex, TestVertex[,] matrix, int i, int j, Random r, List<TestVertex> outEdges)
+        {
+            foreach (var item in directions)
+            {
+                if (r.NextDouble() > 0.5)
+                {
+                    var v = matrix[i + item[0], j + item[1]];
+                    outEdges.Add(v);
+                }
+            }
         }
     }
 
