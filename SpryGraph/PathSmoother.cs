b370d8fe71c960d85f145700f7b6b7b4a5213956
Jack Langman <jlangman@alastri.com.au>
2014-02-24 16:55:22 +0800

    path smoother wip

---
+++ SpryGraph/PathSmoother.cs
@@ -0,0 +1,24 @@
+﻿namespace Alastri.SpryGraph
+{
+    public class PathSmoother<TVertex, TEdge> : IPathFinder<TVertex, TEdge>
+        where TEdge : ICostedEdge<TVertex>
+        where TVertex : IHeuristicVertex<TVertex>
+    {
+        private readonly IPathFinder<TVertex, TEdge> _pathFinder;
+
+        public PathSmoother(IPathFinder<TVertex, TEdge> pathFinder, TVertex source)
+        {
+            _pathFinder = pathFinder;
+            Source = source;
+        }
+
+        public TVertex Source { get; private set; }
+
+        public bool TryGetPath(TVertex destination, out TEdge[] path)
+        {
+            return _pathFinder.TryGetPath(destination, out path);
+        }
+
+
+    }
+}
